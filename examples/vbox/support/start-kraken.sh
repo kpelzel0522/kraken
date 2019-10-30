dbd68fd6d0595aedb35df881caacd0f9a38410d0
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-10-30 12:04:35 -0600

    logging to 10

--- examples/vbox/support/start-kraken.sh
+++ examples/vbox/support/start-kraken.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/vagrant/kraken -log 7 -ip 10.11.12.1 -ipapi 192.168.57.10 >> /home/vagrant/kraken.log 2>&1 &
+/home/vagrant/kraken -log 10 -ip 10.11.12.1 -ipapi 192.168.57.10 >> /home/vagrant/kraken.log 2>&1 &
