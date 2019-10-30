ec8924ff65ed4c5fdf11396abc1d556325abab8e
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-10-30 15:23:43 -0600

    testing

--- modules/testmutate/testmutate.go
+++ modules/testmutate/testmutate.go
@@ -299,22 +299,22 @@ func init() {
 		)
 	}
 
-	// for name, m := range tmuts {
-	// 	dur, _ := time.ParseDuration(m.timeout)
-	// 	mutations[name] = core.NewStateMutation(
-	// 		map[string][2]reflect.Value{
-	// 			TempStateURL: {
-	// 				reflect.ValueOf(m.f),
-	// 				reflect.ValueOf(m.t),
-	// 			},
-	// 		},
-	// 		m.reqs,
-	// 		m.excs,
-	// 		lib.StateMutationContext_CHILD,
-	// 		dur,
-	// 		[3]string{module.Name(), ScalingStateURL, tspb.TestScaling_NONE.String()},
-	// 	)
-	// }
+	for name, m := range tmuts {
+		dur, _ := time.ParseDuration(m.timeout)
+		mutations[name] = core.NewStateMutation(
+			map[string][2]reflect.Value{
+				TempStateURL: {
+					reflect.ValueOf(m.f),
+					reflect.ValueOf(m.t),
+				},
+			},
+			m.reqs,
+			m.excs,
+			lib.StateMutationContext_CHILD,
+			dur,
+			[3]string{module.Name(), ScalingStateURL, tspb.TestScaling_NONE.String()},
+		)
+	}
 
 	si := core.NewServiceInstance("testmutate", module.Name(), module.Entry, nil)
 	// Register it all
