9284ac3f68de57698777a871af01d7c08e1ddd7e
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-10-30 13:41:32 -0600

    readding none to low

--- modules/testmutate/testmutate.go
+++ modules/testmutate/testmutate.go
@@ -52,6 +52,15 @@ type tmut struct {
 }
 
 var muts = map[string]tmut{
+	"NONEtoLOW": {
+		f: tspb.TestScaling_NONE,
+		t: tspb.TestScaling_LOW,
+		reqs: map[string]reflect.Value{
+			"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
+			"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
+		},
+		timeout: "60s",
+	},
 	"NONEtoHIGH": {
 		f: tspb.TestScaling_NONE,
 		t: tspb.TestScaling_HIGH,
