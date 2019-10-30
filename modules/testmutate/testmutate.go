cb5a55693103dbf6dfeb9f88874325d84c822046
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-10-30 16:25:26 -0600

    testing

--- modules/testmutate/testmutate.go
+++ modules/testmutate/testmutate.go
@@ -101,16 +101,16 @@ type mut struct {
 // }
 
 var tmuts = map[string]tmut{
-	// "WARNINGtoNORMAL": {
-	// 	f: tpb.Test_WARNING,
-	// 	t: tpb.Test_NORMAL,
-	// 	reqs: map[string]reflect.Value{
-	// 		"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
-	// 		"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
-	// 		// ScalingStateURL: reflect.ValueOf(tspb.TestScaling_NONE),
-	// 	},
-	// 	timeout: "60s",
-	// },
+	"WARNINGtoNORMAL": {
+		f: tpb.Test_WARNING,
+		t: tpb.Test_NORMAL,
+		reqs: map[string]reflect.Value{
+			"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
+			"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
+			TempStateURL: reflect.ValueOf(tpb.Test_WARNING),
+		},
+		timeout: "60s",
+	},
 	"CRITICALtoWARNING": {
 		f: tpb.Test_CRITICAL,
 		t: tpb.Test_WARNING,
@@ -141,16 +141,16 @@ var tmuts = map[string]tmut{
 		},
 		timeout: "60s",
 	},
-	"NORMALtoCRITICAL": {
-		f: tpb.Test_NORMAL,
-		t: tpb.Test_CRITICAL,
-		reqs: map[string]reflect.Value{
-			"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
-			"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
-			// ScalingStateURL: reflect.ValueOf(tspb.TestScaling_NONE),
-		},
-		timeout: "60s",
-	},
+	// "NORMALtoCRITICAL": {
+	// 	f: tpb.Test_NORMAL,
+	// 	t: tpb.Test_CRITICAL,
+	// 	reqs: map[string]reflect.Value{
+	// 		"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
+	// 		"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
+	// 		// ScalingStateURL: reflect.ValueOf(tspb.TestScaling_NONE),
+	// 	},
+	// 	timeout: "60s",
+	// },
 	"NORMALtoUNKNOWN": {
 		f: tpb.Test_NORMAL,
 		t: tpb.Test_UNKNOWN,
