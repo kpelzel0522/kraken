8f431c4bbe55552dd6f4363971a15d261b5e953c
Kevin Pelzel <kevinpelzel22@gmail.com>
2019-10-30 13:37:24 -0600

    defaults to low power mode

--- modules/testmutate/testmutate.go
+++ modules/testmutate/testmutate.go
@@ -52,23 +52,12 @@ type tmut struct {
 }
 
 var muts = map[string]tmut{
-	"NONEtoHIGH": {
-		f: tspb.TestScaling_NONE,
-		t: tspb.TestScaling_HIGH,
-		reqs: map[string]reflect.Value{
-			"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
-			"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
-			TempStateURL: reflect.ValueOf(tpb.Test_LOW),
-		},
-		timeout: "60s",
-	},
 	"NONEtoLOW": {
 		f: tspb.TestScaling_NONE,
 		t: tspb.TestScaling_LOW,
 		reqs: map[string]reflect.Value{
 			"/PhysState": reflect.ValueOf(cpb.Node_POWER_ON),
 			"/RunState":  reflect.ValueOf(cpb.Node_SYNC),
-			TempStateURL: reflect.ValueOf(tpb.Test_HIGH),
 		},
 		timeout: "60s",
 	},
